fc5a9a774cc618379dd6cf62d5e479b25ed11b57
hrkk <kasper.odgaard@gmail.com>
2021-02-20 09:34:18 +0100

    initial commit

---
+++ src/main/java/dk/ronninit/AreaCalculationController.java
@@ -0,0 +1,47 @@
+package dk.ronninit;
+
+
+import io.github.bucket4j.Bandwidth;
+import io.github.bucket4j.Bucket;
+import io.github.bucket4j.Bucket4j;
+import io.github.bucket4j.Refill;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.Duration;
+
+@Slf4j
+@RestController
+@SpringBootApplication
+public class AreaCalculationController {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AreaCalculationController.class, args);
+    }
+
+    private final Bucket bucket;
+
+    public AreaCalculationController() {
+        Bandwidth limit = Bandwidth.classic(1,  Refill.greedy(1, Duration.ofSeconds(10)));
+        this.bucket = Bucket4j.builder()
+                .addLimit(limit)
+                .build();
+    }
+
+    @PostMapping(value = "/api/v1/area/rectangle")
+    public ResponseEntity<AreaV1> rectangle(@RequestBody RectangleDimensionsV1 dimensions) {
+        if (bucket.tryConsume(1)) {
+            return ResponseEntity.ok(new AreaV1("rectangle", dimensions.getLength() * dimensions.getWidth()));
+        }
+        log.info(HttpStatus.TOO_MANY_REQUESTS.toString());
+        return ResponseEntity.status(HttpStatus.TOO_MANY_REQUESTS).build();
+    }
+
+
+}
---
+++ src/main/java/dk/ronninit/RateLimitingInterceptor.java
@@ -0,0 +1,75 @@
+package dk.ronninit;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
+
+import javax.annotation.PreDestroy;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.Map;
+import java.util.Optional;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+
+@Component
+public class RateLimitingInterceptor extends HandlerInterceptorAdapter {
+
+    private static final Logger logger = LoggerFactory.getLogger(RateLimitingInterceptor.class);
+
+   // @Value("${rate.limit.enabled}")
+    private boolean enabled = true;
+
+    //@Value("${rate.limit.hourly.limit}")
+    private int hourlyLimit = 1;
+
+    private ScheduledExecutorService scheduler = Executors.newScheduledThreadPool(10);
+
+    private Map<String, Optional<SimpleRateLimiter>> limiters = new ConcurrentHashMap<>();
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
+            throws Exception {
+        if (!enabled) {
+            return true;
+        }
+        String clientId = request.getHeader("Client-Id");
+        clientId="020";
+        // let non-API requests pass
+        if (clientId == null) {
+            return true;
+        }
+        SimpleRateLimiter rateLimiter = getRateLimiter(clientId);
+        boolean allowRequest = rateLimiter.tryAcquire();
+
+        if (!allowRequest) {
+            response.setStatus(HttpStatus.TOO_MANY_REQUESTS.value());
+        }
+        response.addHeader("X-RateLimit-Limit", String.valueOf(hourlyLimit));
+        return allowRequest;
+    }
+
+    private SimpleRateLimiter getRateLimiter(String clientId2) {
+        String clientId = clientId2;
+        Optional<SimpleRateLimiter> simpleRateLimiter = limiters.computeIfAbsent(clientId2, applicationId -> Optional.of(createRateLimiter(applicationId)));
+
+        return simpleRateLimiter.get();
+
+    }
+
+    private SimpleRateLimiter createRateLimiter(String applicationId) {
+        logger.info("Creating rate limiter for applicationId={}", applicationId);
+        return SimpleRateLimiter.create(hourlyLimit, TimeUnit.MINUTES); //, scheduler, applicationId);
+    }
+
+
+    @PreDestroy
+    public void destroy() {
+        // loop and finalize all limiters
+        scheduler.shutdown();
+    }
+}
